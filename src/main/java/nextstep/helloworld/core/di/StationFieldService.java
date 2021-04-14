a73d0b9171a30b1518794f4ba875ad948e7fe2fd
naeunkim <knae11@naver.com>
2021-04-14 16:42:38 +0900

    feat: practice Spring Core

--- src/main/java/nextstep/helloworld/core/di/StationFieldService.java
+++ src/main/java/nextstep/helloworld/core/di/StationFieldService.java
@@ -1,7 +1,14 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StationFieldService {
     private StationRepository stationRepository;
+
+    public StationFieldService(StationRepository stationRepository){
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
--- src/main/java/nextstep/helloworld/core/di/StationSetterService.java
+++ src/main/java/nextstep/helloworld/core/di/StationSetterService.java
@@ -1,7 +1,14 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StationSetterService {
     private StationRepository stationRepository;
+
+    public StationSetterService(StationRepository stationRepository){
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
